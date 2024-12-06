1833596fe85b84e5c7c37ede67a2f8cf28b07e52
takei_yn <takei.yn@om.asahi-kasei.co.jp>
2024-12-06 10:22:46 +0900

    takei

--- test_no_format.py
+++ test_no_format.py
@@ -28,7 +28,7 @@ with open(path, encoding="utf-16 LE") as f:
     l = csv.reader(f, delimiter="\t")
     for index_row, row in enumerate(l):
         # 取り込む回答のレコードの数
-        if (index_row < 100) : continue
+        if (index_row > 35) : continue
 
         for index_item, item in enumerate(row):
             # 取り込む回答の項目の数
