ed07eb4a83349fa311012422e6c17899b4eab1e9
AD01\a1059301 <kanamoto.kb@om.asahi-kasei.co.jp>
2024-12-13 13:03:50 +0900

    金本変更

--- join_csv.py
+++ join_csv.py
@@ -5,24 +5,16 @@ import pandas as pd
 path = "./【正式運用版】お客様満足度アンケ.csv"
 path2 = "./物件一覧_20241212133711.csv"
 
-# with open(path, encoding="utf-16 LE") as f:
-#     l = list(csv.reader(f, delimiter="\t"))
-#     for index_row, row in enumerate(l):
+customer_review = pd.read_csv(path, encoding="utf-16 LE", sep="\t")
 
-#         for index_item, item in enumerate(row):
-#             # 取り込む回答の項目の数
-#             # if (index_item < 21) : continue
-#             if (index_item == 2):
-#               # 6桁の番号を抽出
-#               l[index_row][index_item] = item[:6]
+bs_list = pd.read_csv(path2, encoding="CP932")
 
-# print(l)
+for index_row, row in enumerate(customer_review):
+    for index_item, item in enumerate(row):
+        # 取り込む回答の項目の数
+        # if (index_item < 21) : continue
+        if (index_item == 2):
+          # 6桁の番号を抽出
+          customer_review.iloc(index_row, index_item) = item[:6]
 
-l = pd.read_csv(path2, encoding="CP932")
-
-# with open(path2, encoding="shift_jis") as f:
-#     # l = list(csv.reader(f, delimiter="\t"))
-#     l = csv.reader(f, delimiter="\t")
-
-
-print(l)
+print(customer_review)
